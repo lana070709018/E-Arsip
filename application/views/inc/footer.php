373f722fb1bf37390b1216f0db344bdf1d25a38c
technonet\techno <sniko51@yahoo.com>
2018-11-12 09:01:38 +0700

    Noob

---
+++ application/views/inc/footer.php
@@ -0,0 +1,10 @@
+            <footer class="footer ">
+                <div class="container-fluid">
+                    <div class="copyright">
+                        Copyright &copy; <script>document.write(new Date().getFullYear())</script> Nama Perusahaan - Devlop by Kelompok 1 TIF A 2016.
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
